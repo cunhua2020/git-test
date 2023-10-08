47e61b357fee12481af8d49b2656506daff811f8
WuKong <976711063@qq.com>
2023-10-08 17:27:55 +0800

    hot-fix2 commit

--- src/main/java/com/cn/test/gitTest.java
+++ src/main/java/com/cn/test/gitTest.java
@@ -6,5 +6,6 @@ public class gitTest {
         System.out.println("sadas");
         System.out.println("sadas");
         System.out.println("sadas");
+        System.out.println("sadas");
     }
 }
